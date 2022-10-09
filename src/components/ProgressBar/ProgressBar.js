3fb0ab15415d713d0b1e70416e917dc078ae6d17
Desh Deepak Singh <desh.s@iqm.com>
2022-10-09 17:16:51 +0530

    Completed exercise

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,69 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  let Wrapper;
+  switch (size) {
+    case 'small':
+      Wrapper = sPB;
+      break;
+    case 'large':
+      Wrapper = lPB;
+      break;
+    default:
+      Wrapper = mPB;
+  }
+  return (
+    <>
+      <input value={value} type='range' />
+      <Wrapper
+        role='progressbar'
+        aria-valuenow={value}
+        aria-valuemin={'0'}
+        aria-valuemax={'100'}
+      >
+        <BarWrapper>
+          <Mainbar value={value}></Mainbar>
+        </BarWrapper>
+      </Wrapper>
+    </>
+  );
 };
+
+const BaseWrapper = styled.div`
+  border-radius: 4px;
+  min-width: 370px;
+  background-color: ${COLORS.transparentGray15};
+  display: flex;
+  /* overflow: hidden; */
+  align-items: center;
+  justify-content: start;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const sPB = styled(BaseWrapper)`
+  height: 8px;
+`;
+const mPB = styled(BaseWrapper)`
+  height: 12px;
+`;
+const lPB = styled(BaseWrapper)`
+  height: 24px;
+  border-radius: 8px;
+  padding: 4px;
+`;
+
+const BarWrapper = styled.div`
+  overflow: hidden;
+  border-radius: 4px;
+  width: 100%;
+  height: 100%;
+`;
+
+const Mainbar = styled.div`
+  height: 100%;
+  border-radius: 4px 0 0 4px;
+  width: ${(p) => `${p.value}%`};
+  background-color: ${COLORS.primary};
+`;
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useMemo, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
@@ -6,13 +6,79 @@ import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
 const Select = ({ label, value, onChange, children }) => {
+  const [EleW, setEleW] = useState(null);
+  const [EleH, setEleH] = useState(null);
+  const elementRef = useRef();
   const displayedValue = getDisplayedValue(value, children);
+  // const width = useMemo(() => {
+  //   console.log({ elementRef });
+  //   return elementRef?.current?.clientWidth;
+  // }, [elementRef]);
+  // const height = useMemo(() => elementRef?.current?.clientHeight, [elementRef]);
+  let width, height;
 
+  useEffect(() => {
+    if (elementRef) {
+      setEleW(elementRef.current?.clientWidth);
+      setEleH(elementRef?.current?.clientHeight);
+    }
+  }, [elementRef, elementRef.current, displayedValue]);
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect
+        value={value}
+        onChange={onChange}
+        width={EleW}
+        height={EleH}
+        styles={{ '--widhtH': width, '--heightH': height }}
+      >
+        {children}
+      </NativeSelect>
+      <Design ref={elementRef}>
+        <SelectValue>{displayedValue}</SelectValue>
+        <Icon id='chevron-down'></Icon>
+      </Design>
+    </Wrapper>
   );
 };
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  // width: ${(props) => `${props.width}px`}; */
+  // height: ${(p) => `${p.height}px`};
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  /* Allow the select to span the full height in Safari */
+  -webkit-appearance: none;
+`;
+const Design = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  padding: 12px 16px;
+  font-size: 1rem;
+  display: flex;
+  width: fit-content;
+  border-radius: 8px;
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+`;
+const SelectValue = styled.p`
+  margin-right: 24px;
+  display: flex;
+  align-items: center;
+`;
 
 export default Select;
